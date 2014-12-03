4518d1fef5cb0d0a7e0779a8e7edbcf7eadfcfb2
lightball20 <lightball20@yahoo.com>
2014-12-02 20:23:10 -0800

    fixed emails

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 Names:				Emails:
-Kendall Townsend kendallcar@csu.fullerton.edu
-Michael Hatcher
+Kendall Townsend                kendallcar@csu.fullerton.edu
+Michael Hatcher                 lightball20@yahoo.com
 Tommy Phan
 Vinh Ha				vha3@csu.fullerton.edu
 
