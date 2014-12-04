57719085c1e4e28955b80dcdd6af2a7a093172aa
Vinh Ha <vha3@csu.fullerton.edu>
2014-12-03 18:28:03 -0800

    makefile example

---
+++ make/functions.h
@@ -0,0 +1,2 @@
+void printHello();
+int factorial(int n);
---
+++ make/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "functions.h"
+using namespace std;
+
+int main()
+{
+	printHello();
+	cout << endl;
+	cout << "The factorial of 5 is " << factorial(5) << endl;
+
+	return 0; 
+}
