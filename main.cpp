a27a4ad78637c5037c97760f9dbab7916028855c
Tommy Phan <tommyphan8@csu.fullerton.edu>
2014-12-03 20:49:19 -0800

    Added email for Tommy Phan

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 Names:				Emails:
 Kendall Townsend                kendallcar@csu.fullerton.edu
 Michael Hatcher                 lightball20@yahoo.com
-Tommy Phan
+Tommy Phan			tommyphan8@csu.fullerton.edu
 Vinh Ha				vha3@csu.fullerton.edu
 
